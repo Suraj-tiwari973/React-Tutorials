e740513b062429682ae20fa8c5f8b18e0cb4826f
Abhishek Tiwari <tiwarisuraj0852@gmail.com>
2023-02-04 11:15:44 +0530

    learning components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Header from './Header'
+import Footer from './Footer';
+import Main from './Main';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*const app = (
+    <h1>hello sir.</h1>
+)
+ReactDOM.render(app,document.getElementById('root'));*/
+
+// Workinng with components..
+
+function App(){
+    return(
+        <div>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
