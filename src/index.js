b312b4b57b02cac795955568622c5d75771fa28e
Abhishek Tiwari <tiwarisuraj0852@gmail.com>
2023-02-05 14:37:20 +0530

    1

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header'
-import Footer from './Footer';
-import Main from './Main';
-
-
-/*const app = (
-    <h1>hello sir.</h1>
-)
-ReactDOM.render(app,document.getElementById('root'));*/
-
-// Workinng with components..
-
-function App(){
-    return(
-        <div>
-            <Header/>
-            <Main/>
-            <Footer/>
-        </div>
-    )
-}
-
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from './App';
 
 ReactDOM.render(<App/>,document.getElementById('root'));
